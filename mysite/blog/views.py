77f9ac619a327c6a6360b70d0ee41e1a0529a3c9
ecpkngroup8 <47177655+ecpkngroup8@users.noreply.github.com>
2019-01-30 19:47:30 +0800

    Create views.py

---
+++ mysite/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from .models import Blog 
+
+def blog_list(request):
+    context = {}
+    context['blogs'] = Blog.objects.all()
+    return render_to_response('blog_list.html', context)
+
+def blog_detail(request, blog_pk):
+    context = {}
+    context['blog'] = get_object_or_404(Blog, id=blog_pk)
+    return render_to_response('blog_detail.html', context)
